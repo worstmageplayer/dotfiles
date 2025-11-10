da6ab95c2fe6963b41a77bfa91c1b922fff01de4
worstmageplayer <shawnjun05@gmail.com>
2025-11-10 22:51:16 +1100

    output

--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -2,7 +2,7 @@
 
 mkdir -p ~/screenshots
 
-output=~/screenshots/screenshot-$(date +'%Y-%m-%d_%H-%M-%S').png
+output=~/screenshots/$(hyprctl activewindow -j | jq -r '.class')-$(date +'%Y-%m-%d_%H-%M-%S').png
 
 grim -g "$(slurp -b 00000080 -c ffffffff -w 1)" "$output"
 wl-copy < "$output"
