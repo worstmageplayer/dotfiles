4740d1f30846c66f20063189aba920736c0b7c57
worstmageplayer <shawnjun05@gmail.com>
2025-11-10 22:18:50 +1100

    slurp

--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -4,5 +4,5 @@ mkdir -p ~/screenshots
 
 output=~/screenshots/screenshot-$(date +'%Y-%m-%d_%H-%M-%S').png
 
-grim -g "$(slurp)" "$output"
+grim -g "$(slurp -b 00000080 -c ffffffff -w 1)" "$output"
 wl-copy < "$output"
