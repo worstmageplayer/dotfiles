d78569c40e2cfca173119f074e02289eea1988b7
worstmageplayer <shawnjun05@gmail.com>
2025-12-13 13:22:44 +1100

    http

---
+++ scripts/http.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd ~
+PORT="8888"
+IP="$(ip route get 1.1.1.1 2>/dev/null | awk '{for(i=1;i<=NF;i++) if($i=="src") {print $(i+1); exit}}')"
+echo "http://$IP:$PORT/"
+python3 -m http.server $PORT
