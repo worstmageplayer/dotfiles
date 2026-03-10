47b13b8ecb66ebd32478169d907072f2e2767081
worstmageplayer <shawnjun05@gmail.com>
2026-03-10 23:21:09 +1100

    screenshot

--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -4,7 +4,7 @@ mkdir -p ~/screenshots
 
 title=$(hyprctl activewindow -j | jq -r '.title')
 timestamp=$(date +'%Y-%m-%d_%H-%M-%S')
-output="$HOME/screenshots/${title}-${timestamp}.png"
+output="$HOME/screenshots/${timestamp}-${title}.png"
 
 grim -g "$(slurp -b 00000080 -c ffffffff -w 1)" "$output"
 wl-copy < "$output"
