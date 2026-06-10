9f4eb1fbdd432e6b139758a4e9ab8f3cc41f8176
worstmageplayer <shawnjun05@gmail.com>
2026-06-11 00:55:31 +1000

    binds

--- hypr/hyprland/binds.lua
+++ hypr/hyprland/binds.lua
@@ -87,17 +87,22 @@ hl.bind(mainMod .. " + SHIFT + J",     hl.dsp.window.swap({ direction = "up" }))
 hl.bind(mainMod .. " + SHIFT + K",     hl.dsp.window.swap({ direction = "down" }))
 hl.bind(mainMod .. " + SHIFT + L",     hl.dsp.window.swap({ direction = "right" }))
 
+local function workspaceFocus(i)
+  hl.dispatch(hl.dsp.focus({ workspace = i}))
+  local workspace = hl.get_active_workspace()
+  if workspace and workspace.id == i then
+    hl.dispatch(hl.dsp.window.cycle_next())
+  end
+end
 -- Switch workspaces with mainMod + [0-9]
 -- Move active window to a workspace with mainMod + SHIFT + [0-9]
 for i = 1, 10 do
     local key = i % 10 -- 10 maps to key 0
-    hl.bind(mainMod .. " + " .. key,             hl.dsp.focus({ workspace = i}))
+    hl.bind(mainMod .. " + " .. key,             function()
+      workspaceFocus(i)
+    end)
     hl.bind(mainMod .. " + SHIFT + " .. key,     hl.dsp.window.move({ workspace = i }))
 end
-
--- Scroll through existing workspaces with mainMod + scroll
-hl.bind(mainMod .. " + mouse_down", hl.dsp.focus({ workspace = "e-1" }))
-hl.bind(mainMod .. " + mouse_up",   hl.dsp.focus({ workspace = "e+1" }))
 
 -- Move/resize windows with mainMod + LMB/RMB and dragging
 hl.bind(mainMod .. " + mouse:272", hl.dsp.window.drag(),   { mouse = true })
